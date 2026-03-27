02962c1cb97b794cadaa3ffdf88f62df9e44327d
LeoKaichou <tnamgyal582005@gmail.com>
2026-03-27 22:04:17 +0530

    Add files via upload

---
+++ album/js/playlist.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: "Bite Me",
+    artist: "Enhypen",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Fate",
+    artist: "Enhypen",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Sacrifice",
+    artist: "Enhypen",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Bills",
+    artist: "Enhypen",
+    img: "img4",
+    audio: "music4"
+  },
+  {
+    name: "Karma",
+    artist: "Enhypen",
+    img: "img4",
+    audio: "music5"
+  },
+  {
+    name: "Chaconne",
+    artist: "Enhypen",
+    img: "img4",
+    audio: "music6"
+  }
+]
